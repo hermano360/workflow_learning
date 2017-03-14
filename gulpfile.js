eb6b85642f5c7e4f4dc9d90fb749b8bd31b4a57d
Herminio Garcia <hermano360@gmail.com>
2017-03-14 01:42:30 -0700

    added compass plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
+    compass = require('gulp-compass'),
+    sass = require('gulp-sass'),
     browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -10,7 +12,9 @@ var jsSources = [
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
-]
+];
+var sassSources = ['components/sass/style.scss'];
+
 gulp .task('coffee',function() {
    gulp.src(coffeeSources)
    	.pipe(coffee({ bare:true })
@@ -24,3 +28,15 @@ gulp.task('js', function(){
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
+});
+
